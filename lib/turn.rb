db47992887b1660dac81afa307989d17ae531807
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-02 22:04:27 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,11 @@
 
 
 
+def position_taken?(board, index)
+   (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
+    false : true
+end
+
 def valid_move?(board, index)
   if index.between?(0, 8) && !position_taken?(board, index) 
   
