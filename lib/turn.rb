df70852615cc4522f3ff1d6df030eddc2de9e68f
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-02 22:21:15 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -30,17 +30,17 @@ def valid_move?(board, index)
   end
 end
 
-def turn(board)
-  puts "Please enter 1-9:"
-  input = get.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
-  end
-end
+# def turn(board)
+#   puts "Please enter 1-9:"
+#   input = get.strip
+#   index = input_to_index(input)
+#   if valid_move?(board, index)
+#     move(board, index)
+#     display_board(board)
+#   else
+#     turn(board)
+#   end
+# end
 
 def turn(board)
   puts "Please enter 1-9:"
